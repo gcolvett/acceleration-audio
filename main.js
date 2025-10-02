ee4a5e5762fccba12d8a16057e07a8ffd66360e9
gcolvett <gcolvett@charter.net>
2025-10-02 17:45:59 -0500

    Initial commit

--- main.js
+++ main.js
@@ -1,47 +1,147 @@
 const audioPlayer = document.getElementById("audioPlayer");
+const albumArt = document.getElementById("albumArt");
+const songTitle = document.getElementById("songTitle");
 const playButton = document.getElementById("playButton");
 const pauseButton = document.getElementById("pauseButton");
-const progressBar = document.getElementById("progress");
-const customProgressBar = document.getElementById("custom-progress-bar");
+const skipButton = document.getElementById("skipButton");
+const backButton = document.getElementById("backButton");
+const progressBar = document.getElementById("progressBar");
+const progressFill = document.getElementById("progressFill");
+const currentSpeed = document.getElementById("currentSpeed");
 
-const baseURL = "/songs";
-
+let startup = true;
 let currentTrack = 0;
 
-const songs = [
-  { file: "Aidan.mp3", albulmArt: "Aiden.jpg" },
-  { file: "autumn_sun.mp3", albulmArt: "BestPart.jpg" },
-];
+audioPlayer.playbackRate = 1.0;
 
-playButton.addEventListener("click", () => {
-  audioPlayer.src = `${baseURL}/${songs[currentTrack].file}`;
+const songs = [
+  { file: "Better Days - LAKEY INSPIRED.mp3", songTitle: "Better Days", albumArt: "Better Days.jpg" },
+  { file: "autumn_sun.mp3", songTitle: "Autumn Sun", albumArt: "autumn_sun.png" },
+  { file: "Polarity.mp3", songTitle: "Polarity", albumArt: "Polarity.jpg" },
+]
+
+function loadTrack(currentTrack) {
+  audioPlayer.src = `/songs/${songs[currentTrack].file}`;
   audioPlayer.load();
-  audioPlayer.play();
+  audioPlayer.playbackRate = 1.0;
+  currentSpeed.textContent = "1.0x";
+
+  albumArt.src = `/albumart/${songs[currentTrack].albumArt}`;
+  songTitle.textContent = songs[currentTrack].songTitle;
+}
+
+// play button
+playButton.addEventListener("click", () => {
+
+  playButton.classList.add("hidden")
+  pauseButton.classList.remove("hidden")
+
+  // on initial startup
+  if (startup === true) {
+    startup = false;
+
+    loadTrack(currentTrack);
+    audioPlayer.play();
+    
+    albumArt.classList.remove("invisible")
+  // after initial startup
+  } else {
+    audioPlayer.play()
+  }
 });
 
-pauseButton.addEventListener("click", () => {
-  console.log("pause");
+// pause button
+pauseButton.addEventListener("click", () => {  
+  // console.log("pause");
+  pauseButton.classList.add("hidden")
+  playButton.classList.remove("hidden")
+
   audioPlayer.pause();
 });
 
-audioPlayer.addEventListener("timeupdate", () => {
-  progressBar.value = audioPlayer.currentTime / audioPlayer.duration;
-  customProgressBar.style.width = `${
-    (audioPlayer.currentTime / audioPlayer.duration) * 100
-  }%`;
+// skip button
+skipButton.addEventListener("click", () => {
+  // console.log("skip");
+  currentTrack += 1;
 
-  console.log(`${audioPlayer.currentTime} / ${audioPlayer.duration}`);
+  // loop if at end of list
+  if (currentTrack >= songs.length) {
+    currentTrack = 0;
+  }
 
-  audioPlayer.playbackRate += 0.01;
+  // load next song
+  loadTrack(currentTrack);
+  
+  // autoplay if not paused
+  if (!audioPlayer.paused) {
+    audioPlayer.play();
+  }
 });
 
-document.addEventListener("keydown", (event) => {
-  switch (event.key.toLowerCase()) {   
-    case " ":
-      event.preventDefault();
-      audioPlayer.paused ? audioPlayer.play() : audioPlayer.pause();
-      break;
+// back button
+backButton.addEventListener("click", () => {
+  // console.log("back")
+  
+  if (audioPlayer.paused) {
+    currentTrack -= 1;
+  
+  // loop if at beginning of list
+    if (currentTrack < 0) {
+      currentTrack = songs.length - 1;
+    }
 
+    loadTrack(currentTrack);
+
+  } else {
+    audioPlayer.load()
+
+    pauseButton.classList.add("hidden")
+    playButton.classList.remove("hidden")
+  }
+});
+
+// autoplay next song when a song ends
+audioPlayer.addEventListener("ended", () => {
+  currentTrack += 1;
+
+  // loop if at end of list
+  if (currentTrack >= songs.length) {
+    currentTrack = 0;
+  }
+  loadTrack(currentTrack);
+  audioPlayer.play();
+});
+
+// speed up song
+audioPlayer.addEventListener("timeupdate", () => {
+  progressBar.value = audioPlayer.currentTime / audioPlayer.duration;
+  progressFill.style.width = `${(audioPlayer.currentTime / audioPlayer.duration) * 100}%`;
+
+  // increase speed by 0.01 and cap at 4.0x
+  audioPlayer.playbackRate = Math.min(
+    Math.round((audioPlayer.playbackRate + 0.01) * 100) / 100, 4.0
+  );
+
+  // show current speed
+  currentSpeed.textContent = `${audioPlayer.playbackRate.toFixed(1)}x`;
+});
+
+// keybinds
+document.addEventListener("keydown", (event) => {
+  switch (event.key.toLowerCase()) {
+    case " ":
+      if (audioPlayer.paused && startup === false) {
+        audioPlayer.play()
+
+        playButton.classList.add("hidden")
+        pauseButton.classList.remove("hidden")
+      } else if (!audioPlayer.paused) {
+        audioPlayer.pause()
+        
+        pauseButton.classList.add("hidden")
+        playButton.classList.remove("hidden")
+      }
+      break;
     case "m":
       audioPlayer.muted = !audioPlayer.muted;
       break;
